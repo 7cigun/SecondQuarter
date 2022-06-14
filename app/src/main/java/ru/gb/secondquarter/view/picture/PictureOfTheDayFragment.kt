cd32f90151ccd490c3867a042da0f1ad82b04f94
7cigun <7cigun@gmail.com>
2022-06-14 19:33:32 +0300

    Урок 1. Добавил Chip.

--- app/src/main/java/ru/gb/secondquarter/view/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/ru/gb/secondquarter/view/picture/PictureOfTheDayFragment.kt
@@ -7,11 +7,14 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.load
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.chip.Chip
 import ru.gb.secondquarter.R
 import ru.gb.secondquarter.databinding.BottomSheetLayoutBinding
 import ru.gb.secondquarter.databinding.FragmentPictureOfTheDayBinding
@@ -21,17 +24,21 @@ import ru.gb.secondquarter.viewmodel.PictureOfTheDayViewModel
 
 class PictureOfTheDayFragment : Fragment() {
 
-    var _binding : FragmentPictureOfTheDayBinding? = null
-    val binding: FragmentPictureOfTheDayBinding
-    get() = _binding!!
+    var isMain = true
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    var _binding: FragmentPictureOfTheDayBinding? = null
+    val binding: FragmentPictureOfTheDayBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentPictureOfTheDayBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    private val viewModel : PictureOfTheDayViewModel by lazy {
+    private val viewModel: PictureOfTheDayViewModel by lazy {
         ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
     }
 
@@ -41,10 +48,21 @@ class PictureOfTheDayFragment : Fragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.app_bar_fav -> {Toast.makeText(requireContext().applicationContext,"Нажата кнопка Избранное", Toast.LENGTH_SHORT).show()}
-            R.id.app_bar_settings -> {Toast.makeText(context,"Нажата кнопка Настройки", Toast.LENGTH_SHORT).show()}
-            android.R.id.home -> { BottomNavigationDrawerFragment.newInstance().show(requireActivity().supportFragmentManager, "") }
+        when (item.itemId) {
+            R.id.app_bar_fav -> {
+                Toast.makeText(
+                    requireContext().applicationContext,
+                    "Нажата кнопка Избранное",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            R.id.app_bar_settings -> {
+                Toast.makeText(context, "Нажата кнопка Настройки", Toast.LENGTH_SHORT).show()
+            }
+            android.R.id.home -> {
+                BottomNavigationDrawerFragment.newInstance()
+                    .show(requireActivity().supportFragmentManager, "")
+            }
         }
         return super.onOptionsItemSelected(item)
     }
@@ -58,11 +76,13 @@ class PictureOfTheDayFragment : Fragment() {
         viewModel.sendRequest()
 
         makeIconSearch()
-
         makeBottomSheet()
+        runBottomBar()
+        checkIsMain()
+        chipSet()
     }
 
-    private fun makeIconSearch(){
+    private fun makeIconSearch() {
         binding.inputLayout.setEndIconOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW).apply {
                 data =
@@ -72,14 +92,14 @@ class PictureOfTheDayFragment : Fragment() {
         }
     }
 
-    private fun makeBottomSheet(){
+    private fun makeBottomSheet() {
         val bottomSheetBehavior = BottomSheetBehavior.from(binding.bindingHack.bottomSheetContainer)
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
-        bottomSheetBehavior.addBottomSheetCallback( object :
-            BottomSheetBehavior.BottomSheetCallback(){
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
                 when (newState) {
-                    BottomSheetBehavior.STATE_DRAGGING ->{}
+                    BottomSheetBehavior.STATE_DRAGGING -> {}
                     BottomSheetBehavior.STATE_COLLAPSED -> {}
                     BottomSheetBehavior.STATE_EXPANDED -> {}
                     BottomSheetBehavior.STATE_HALF_EXPANDED -> {}
@@ -89,23 +109,71 @@ class PictureOfTheDayFragment : Fragment() {
             }
 
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                Log.d("@@@","$slideOffset")
+                Log.d("@@@", "$slideOffset")
             }
 
         })
+    }
 
+    private fun runBottomBar() {
         (requireActivity() as MainActivity).setSupportActionBar(binding.bottomAppBar)
         setHasOptionsMenu(true)
     }
 
-    private fun renderData(pictureOfTheDayAppState : PictureOfTheDayAppState){
-        when(pictureOfTheDayAppState) {
+    private fun checkIsMain() {
+        binding.fab.setOnClickListener {
+            if (isMain) {
+                binding.bottomAppBar.navigationIcon = null
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+                binding.fab.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_back_fab
+                    )
+                )
+                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_blank)
+            } else {
+                binding.bottomAppBar.navigationIcon = (ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.ic_hamburger_menu_bottom_bar
+                ))
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+                binding.fab.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_plus_fab
+                    )
+                )
+                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar)
+            }
+            isMain = !isMain
+        }
+    }
+
+    private fun chipSet() {
+        binding.chipGroup.setOnCheckedChangeListener { group, position ->
+            /* TODO HW
+             when(position){
+                1->{viewModel.sendRequestToday()}
+                2->{viewModel.sendRequestYT()}
+                3->{viewModel.sendRequestTDBY()}
+            }*/
+            group.findViewById<Chip>(position)?.let {
+                Log.d("@@@", "${it.text.toString()} $position")
+            }
+        }
+    }
+
+    private fun renderData(pictureOfTheDayAppState: PictureOfTheDayAppState) {
+        when (pictureOfTheDayAppState) {
             is PictureOfTheDayAppState.Error -> {}
             is PictureOfTheDayAppState.Loading -> {}
             is PictureOfTheDayAppState.Success -> {
                 binding.imageView.load(pictureOfTheDayAppState.pictureOfTheDayResponseData.url)
-                binding.bindingHack.title.text = pictureOfTheDayAppState.pictureOfTheDayResponseData.title
-                binding.bindingHack.explanation.text = pictureOfTheDayAppState.pictureOfTheDayResponseData.explanation
+                binding.bindingHack.title.text =
+                    pictureOfTheDayAppState.pictureOfTheDayResponseData.title
+                binding.bindingHack.explanation.text =
+                    pictureOfTheDayAppState.pictureOfTheDayResponseData.explanation
             }
         }
     }
