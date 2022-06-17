5b4954b09c784bfca5000f106b2de3acbc5de587
7cigun <7cigun@gmail.com>
2022-06-17 11:36:41 +0300

    Урок 2. Создал 3 темы приложения.

--- app/src/main/java/ru/gb/secondquarter/view/MainActivity.kt
+++ app/src/main/java/ru/gb/secondquarter/view/MainActivity.kt
@@ -8,6 +8,7 @@ import ru.gb.secondquarter.view.picture.PictureOfTheDayFragment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.LimeTheme)
         setContentView(R.layout.activity_main)
         if(savedInstanceState==null){
             supportFragmentManager.beginTransaction().replace(R.id.container,PictureOfTheDayFragment.newInstance()).commit()
